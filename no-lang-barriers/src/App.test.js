9a2133d285954e4e769067d898a29cc57c7441b2
Ivan <ivanilev95@gmail.com>
2022-11-06 13:20:08 +0200

    Clean up

--- no-lang-barriers/src/App.test.js
+++ no-lang-barriers/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const paragraphElement = screen.getByText(/A new website is coming soon.../i);
+  expect(paragraphElement).toBeInTheDocument();
 });
